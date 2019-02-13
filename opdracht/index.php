c1644cdefc61a693716809408298bef461431af0
Mats <99052518@mydavinci.nl>
2019-02-13 09:19:39 +0100

    added dynamic time

--- opdracht/index.php
+++ opdracht/index.php
@@ -38,11 +38,11 @@ switch ($time) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous"></script>
   <title>Goedemorgen</title>
 </head>
-<body>
+<body onLoad="startTime();">
   <style>
     body {
       background-image: url(<?=$img?>);
@@ -58,10 +58,26 @@ switch ($time) {
       margin-top: 20%;
     }
   </style>
+  <script>
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+  return i;
+  }
+
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var hour = checkTime(h)
+    var min = checkTime(m);
+    document.getElementById('clock').innerHTML = "Het is nu: " + hour + ":" + min; 
+    var t = setTimeout(startTime, 500);
+}
+  </script>
   <?php include '../components/navbar.html'; ?>
   <div class="main">
     <h1>Goede <?=$msg?>!</h1>
-    <h1>Het is nu: <?=$full_time?></h1>
+    <h1 id="clock"></h1>
   </div>
 </body>
 </html>
